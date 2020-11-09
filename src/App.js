b499574d7623ce3a3e4abc0a1ca9489c7248d818
monsever13 <avanza_cs1@rocketmail.com>
2020-11-09 11:39:27 +0700

    add redux to cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// feature 1
 import React from "react";
-import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
@@ -8,66 +6,26 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: data.products,
-      cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")) : [],
-     
-    };
-  }
-  createOrder = (order) =>{
-    alert ("Need to save order for " + order.name );
-  };
-
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((x) => x._id !== product._id),
-    });
-    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)));
-  };
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-    cartItems.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-      cartItems.push({ ...product, count: 1 });
-    }
-    this.setState({ cartItems });
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  };
-  
-  
   render() {
     return (
       <Provider store={store}>
-      <div className="grid-container">
-        <header>
-          <a href="/">Bilqis Store</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter></Filter>
-              <Products addToCart={this.addToCart}></Products>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter></Filter>
+                <Products></Products>
+              </div>
+              <div className="sidebar">
+                <Cart />
+              </div>
             </div>
-            <div className="sidebar">
-              <Cart
-                cartItems={this.state.cartItems}
-                removeFromCart={this.removeFromCart}
-                createOrder={this.createOrder}
-              />
-            </div>
-          </div>
-        </main>
-        <footer>Powered by MSuprayogie</footer>
-      </div>
+          </main>
+          <footer>All right is reserved.</footer>
+        </div>
       </Provider>
     );
   }
